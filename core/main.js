b589ce76d7be875fd0cc6eef6d1194f4e708ca2b
KwongTN <tungnan5636@gmail.com>
2020-04-15 22:26:10 +0800

    Create main.js with transcript support

---
+++ core/main.js
@@ -0,0 +1,32 @@
+const transcript = require("./transcript/transcript.js");
+const fs = require("fs");
+
+var myJSON;
+
+// You should pass a HAR json here
+
+
+// Read the HAR file
+try {
+    myJSON = JSON.parse(fs.readFileSync("./app.pluralsight.com_transcript.har"));
+} catch (err) {
+    console.log(err.message);
+}
+
+// Create output directory if it doesn't exist
+if (!fs.existsSync("./output")) {
+    try {
+        fs.mkdirSync("./output");
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+// Generate transcript
+const searchString = /https:\/\/app\.pluralsight\.com\/learner\/user\/courses.*transcript/
+myJSON.log.entries.forEach((element, index) => {
+    if (searchString.test(element.request.url)){
+        const passedJSON = JSON.parse(element.response.content.text);
+        transcript.generateTranscript(passedJSON);
+    }
+});
