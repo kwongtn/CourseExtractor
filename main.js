5b9596fdb405327beaa51a8592d6d20a00902bbb
KwongTN <tungnan5636@gmail.com>
2020-04-16 13:10:08 +0800

    Update main with respect to passed parameters

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 const transcript = require("./functions/transcript.js");
 const courseInfo = require("./functions/courseinfo.js");
+const paramProcessor = require("./functions/paramProcessor.js")
 const fs = require("fs");
 
 var myJSON;
 
-// Passed params
-console.log("Passed params: ");
-process.argv.forEach((value, index) => {
-    console.log(value, index);
-})
-
+const params = paramProcessor.parse(process.argv);
+console.log(params);
 
 // Read the HAR file
 try {
-    myJSON = JSON.parse(fs.readFileSync("./app.pluralsight.com_transcript.har"));
+    myJSON = JSON.parse(fs.readFileSync(params.path));
 } catch (err) {
     console.log(err.message);
+    process.exit();
 }
 
 // Create output directory if it doesn't exist
@@ -24,61 +22,86 @@ if (!fs.existsSync("./output")) {
         fs.mkdirSync("./output");
     } catch (err) {
         console.log(err.message);
+        process.exit();
     }
 }
 
 // Generate transcript
-const searchString = /https:\/\/app\.pluralsight\.com\/learner\/user\/courses.*transcript/;
-var obtainedTranscript = false
-myJSON.log.entries.forEach((element, index) => {
-    if (!obtainedTranscript && searchString.test(element.request.url)) {
-        const passedJSON = JSON.parse(element.response.content.text);
-        transcript.generateTranscript(passedJSON);
-        obtainedTranscript = true;
+if (!params.noSubs) {
+    const searchString = /https:\/\/app\.pluralsight\.com\/learner\/user\/courses.*transcript/;
+    var obtainedTranscript = false
+    try {
+        myJSON.log.entries.forEach((element, index) => {
+            if (!obtainedTranscript && searchString.test(element.request.url)) {
+                const passedJSON = JSON.parse(element.response.content.text);
+                transcript.generateTranscript(passedJSON);
+                obtainedTranscript = true;
+            }
+        });
+    } catch (err) {
+        console.log(err.message);
+        console.log("If you see this its probably because the HAR file you provided does not have a transcript, or that the format has changed.");
+        console.log("If you are sure that the format has changed, please open an issue here: https://github.com/kwongtn/CourseExtractor/issues");
     }
-});
+}
 
 // Generate and write courseInfo to output
-const searchString = /https:\/\/app\.pluralsight\.com\/learner\/content\/courses.*/
-var obtainedCourseInfo = false;
-myJSON.log.entries.forEach((element, index) => {
-    if (!obtainedCourseInfo && searchString.test(element.request.url)) {
-        const passedJSON = JSON.parse(element.response.content.text);
-        courseInfo.courseInfoTxt(passedJSON).then((output) => {
-            try {
-                fs.writeFileSync("./output/courseInfo_" + output.courseInfo.id + ".txt", output.text);
-                console.log("Completed course output for " + output.courseInfo.id);
-            } catch (err) {
-                console.log(err);
-                return false;
+if (!params.noInfo) {
+    const searchString = /https:\/\/app\.pluralsight\.com\/learner\/content\/courses.*/
+    var obtainedCourseInfo = false;
+    try {
+        myJSON.log.entries.forEach((element, index) => {
+            if (!obtainedCourseInfo && searchString.test(element.request.url)) {
+                const passedJSON = JSON.parse(element.response.content.text);
+                courseInfo.courseInfoTxt(passedJSON).then((output) => {
+                    try {
+                        fs.writeFileSync("./output/courseInfo_" + output.courseInfo.id + ".txt", output.text);
+                        console.log("Completed course output for " + output.courseInfo.id);
+                    } catch (err) {
+                        console.log(err);
+                        return false;
+                    }
+    
+                    return true;
+                });
+    
+                obtainedCourseInfo = true;
             }
-
-            return true;
         });
-
-        obtainedCourseInfo = true;
+    } catch (err){
+        console.log(err.message);
+        console.log("If you see this its probably because the HAR file you provided does not have course info, or that the format has changed.");
+        console.log("If you are sure that the format has changed, please open an issue here: https://github.com/kwongtn/CourseExtractor/issues")
     }
-});
+
+}
 
 // Generate and write course bb code to output
-// TODO: searchString
-const searchString = /https:\/\/app\.pluralsight\.com\/learner\/content\/courses.*/
-var obtainedCourseInfoBb = false;
-myJSON.log.entries.forEach((element, index) => {
-    if (!obtainedCourseInfoBb && searchString.test(element.request.url)) {
-        const passedJSON = JSON.parse(element.response.content.text);
-        courseInfo.courseInfoTxt(passedJSON).then((output) => {
-            try {
-                fs.writeFileSync("./output/courseBb_" + output.courseInfo.id + ".txt", output.text);
-                console.log("Completed bb text output for " + output.courseInfo.id);
-            } catch (err) {
-                console.log(err);
-                return false;
+if (!params.noBB) {
+    const searchString = /https:\/\/app\.pluralsight\.com\/learner\/content\/courses.*/
+    var obtainedCourseInfoBb = false;
+    try{
+        myJSON.log.entries.forEach((element, index) => {
+            if (!obtainedCourseInfoBb && searchString.test(element.request.url)) {
+                const passedJSON = JSON.parse(element.response.content.text);
+                courseInfo.courseInfoTxt(passedJSON).then((output) => {
+                    try {
+                        fs.writeFileSync("./output/courseBb_" + output.courseInfo.id + ".txt", output.text);
+                        console.log("Completed bb text output for " + output.courseInfo.id);
+                    } catch (err) {
+                        console.log(err);
+                        return false;
+                    }
+    
+                    return true;
+                });
+    
+                obtainedCourseInfoBb = true;
             }
-
-            return true;
         });
-
-        obtainedCourseInfoBb = true;
+    } catch (err){
+        console.log(err.message);
+        console.log("If you see this its probably because the HAR file you provided does not have course info, or that the format has changed.");
+        console.log("If you are sure that the format has changed, please open an issue here: https://github.com/kwongtn/CourseExtractor/issues")
     }
-});
+}
