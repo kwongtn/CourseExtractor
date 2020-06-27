af5936f02f214364a862d8b4caacd1804f97ae37
KwongTN <tungnan5636@gmail.com>
2020-06-27 20:08:46 +0800

    Fixed on Pluralsight update where duplicate URLs were given

--- functions/video.js
+++ functions/video.js
@@ -2,12 +2,12 @@
  * 
  * @param {array} entries 
  */
-function getClipID(entries){
+function getClipID(entries) {
     return new Promise((resolve) => {
         entries.forEach((entry) => {
             const searchString = /https:\/\/vid.pluralsight.com\/clips\/resolution\/.*/;
 
-            if(searchString.test(entry.url)){
+            if (searchString.test(entry.url)) {
                 var returnURL = entry.url.replace(/https:\/\/vid.pluralsight.com\/clips\/resolution\//g, "");
                 returnURL = returnURL.replace(/\/current\/mp4\/.*/g, "");
                 console.log(returnURL);
@@ -23,7 +23,6 @@ function getClipID(entries){
     })
 }
 
-
 /**
  * Outputs video links as array, together with stuff for subtitles.
  * @param {Object} linkJSON - JSON containing all video links.
@@ -32,7 +31,8 @@ function getClipID(entries){
 function getURLs(linkJSON, extractAll = false) {
     const searchString = /https:\/\/app.pluralsight.com\/video\/clips\/v3\/viewclip.*/;
     var URLs = [];
-    linkJSON.log.entries.forEach(async(element, index) => {
+    var history = [];
+    linkJSON.log.entries.forEach(async (element, index) => {
         try {
             if (searchString.test(element.request.url)) {
                 const passedJSON = JSON.parse(element.response.content.text);
@@ -44,22 +44,27 @@ function getURLs(linkJSON, extractAll = false) {
                     myJSON.version = passedJSON.version;
                     myJSON.sessionID = passedJSON.sessionId;
                     myJSON.videoID = videoID;
+                    
+                    if(!history.includes(myJSON.url)){
+                        history.push(myJSON.url);
+                        URLs.push(myJSON);
 
-                    URLs.push(myJSON);
+                    }
+
                 } else {
                     console.log("Extracting all URLs.");
                     passedJSON.urls.forEach((urlGroup, index) => {
                         URLs.push(urlGroup.url);
                     });
                 }
-    
+
             }
         } catch (err) {
             console.log(err.message);
             console.log("If you are seeing this, it means that your HAL file was not complete with the video links.");
         }
     });
-    
+
     return URLs;
 }
 
